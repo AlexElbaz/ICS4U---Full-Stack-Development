9629a017a0a6d51dfca02b35e6a330a5a5a7f8d9
Alex Elbaz <aelbaz@bayviewglen.ca>
2021-10-19 10:47:00 -0400

    Small changes to html files (detailing and getting rid of unnecessary code), some new css styles (though not fully developed yet), and more work on the match log page (Fixed some errors/redundancies so the table displays properly all the time, figured out the pagination (can now use buttons to refresh the table with the next set of 10 games), and started on the formatting of the table so it is actually readable and nice to look at).

--- Introduction to JavaScript/src/SportsStatisticsApplication/matches.js
+++ Introduction to JavaScript/src/SportsStatisticsApplication/matches.js
@@ -1,69 +1,95 @@
-function getMatches(){
+let matches = [];
+pageNum = 1;
+
+function getTenMatches(){
+    document.querySelector(`#btn${pageNum}`).classList.add("active");
     fetch('matchStatistics.json')
     .then((res) => res.json())
     .then((data) => {
       data.forEach((match) => {
-        let row = createRow(match);
-        document.querySelector('#rows').appendChild(row);
+        let body = createRow(match);
+        matches.push(body);
+        if (matches.length > (pageNum - 1) * 10 && matches.length <= pageNum * 10)
+            document.querySelector('#rows').appendChild(body);
       });
     })
-  }
-
-getMatches();
+}
 
 function createRow(match) {
-    let newRow = document.createElement('tr');
-    newRow.setAttribute('scope', 'row');
+    let newBody = document.createElement('tbody');
+    newBody.setAttribute('scope', 'body');
+    newBody.classList.add('borderless', 'card');        // FINISH BORDERLESS!!!! CURRENTLY DOESN'T WORK (IN CSS)
+    let firstNewRow = document.createElement('tr');
+    firstNewRow.classList.add('card-title', 'align-self-center');
+    firstNewRow.setAttribute('scope', 'row');
     let newCell = document.createElement('td');
     newCell.appendChild(document.createTextNode(match.home));
-    newRow.appendChild(newCell);
-    newCell = document.createElement('td');
-    newCell.appendChild(document.createTextNode(match.away));
-    newRow.appendChild(newCell);
+    firstNewRow.appendChild(newCell);
     newCell = document.createElement('td');
     newCell.appendChild(document.createTextNode(match.homeSets));
-    newRow.appendChild(newCell);
+    firstNewRow.appendChild(newCell);
     newCell = document.createElement('td');
     newCell.appendChild(document.createTextNode(match.awaySets));
-    newRow.appendChild(newCell);
+    firstNewRow.appendChild(newCell);
+    newCell = document.createElement('td');
+    newCell.appendChild(document.createTextNode(match.away));
+    firstNewRow.appendChild(newCell);
+    newBody.appendChild(firstNewRow);
+    
+    let secondNewRow = document.createElement('tr');
+    secondNewRow.setAttribute('scope', 'row');
+    secondNewRow.classList.add('card-body', 'align-self-center');
     newCell = document.createElement('td');
     newCell.appendChild(document.createTextNode(match.gameOne));
-    newRow.appendChild(newCell);
+    secondNewRow.appendChild(newCell);
     newCell = document.createElement('td');
     newCell.appendChild(document.createTextNode(match.gameTwo));
-    newRow.appendChild(newCell);
+    secondNewRow.appendChild(newCell);
     newCell = document.createElement('td');
     newCell.appendChild(document.createTextNode(match.gameThree));
-    newRow.appendChild(newCell);
-    newCell = document.createElement('td');
-    newCell.appendChild(document.createTextNode(match.gameFour));
-    newRow.appendChild(newCell);
-    newCell = document.createElement('td');
-    newCell.appendChild(document.createTextNode(match.gameFive));
-    newRow.appendChild(newCell);
+    secondNewRow.appendChild(newCell);
+    if (match.gameFour != null) {
+        newCell = document.createElement('td');
+        newCell.appendChild(document.createTextNode(match.gameFour));
+        secondNewRow.appendChild(newCell);
+        if (match.gameFive != null) {
+            newCell = document.createElement('td');
+            newCell.appendChild(document.createTextNode(match.gameFive));
+            secondNewRow.appendChild(newCell);
+        }
+    }
+    newBody.appendChild(secondNewRow)
 
-    return newRow;
+    return newBody;
 }
 
 function changePage(newPage) {
-    let id = "btn" + (pageNum+1);
-    document.querySelector(`#${id}`).classList.add("btn-secondary");
+    document.querySelector(`#btn${pageNum}`).classList.remove("active");
     pageNum = newPage;
-    getTenUsers();
+    matches = [];
+    document.querySelector('#rows').innerHTML = '';
+    getTenMatches();
 }
 
 function previousPage() {
-    let id = "btn" + (pageNum+1);
-    document.querySelector(`#${id}`).classList.add("btn-secondary");
-    if (pageNum > 1)
+    if (pageNum > 1) {
+        document.querySelector(`#btn${pageNum}`).classList.remove("active");
         pageNum -= 1;
-    getTenUsers();
+        matches = [];
+        document.querySelector('#rows').innerHTML = '';
+        getTenMatches();
+    }
 }
 
 function nextPage() {
-    let id = "btn" + (pageNum+1);
-    document.querySelector(`#${id}`).classList.add("btn-secondary");
-    if (pageNum < userRecords.length/10)
+    if (pageNum < matches.length/10) {
+        document.querySelector(`#btn${pageNum}`).classList.remove("active");
         pageNum += 1;
-    getTenUsers();
+        matches = [];
+        document.querySelector('#rows').innerHTML = '';
+        getTenMatches();
+    }
 }
+
+
+getTenMatches();
