676178e2bdabf128dfd11d3d546aa03794161bcd
Alex Elbaz <aelbaz@bayviewglen.ca>
2021-11-04 09:29:20 -0400

    Vue sample-project - First Vue Project. Rebuilding Task Tracker project we watched in class. Completed basic layout of website and vue files and emitting/handling events (like click events).

---
+++ Introduction to Vue/sample-project/src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <h1 :style="{color:color}">{{title}}</h1>
+        <Button :msg="msg" />
+        
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+  name: 'Header',
+  components: {
+      Button
+  },
+  props: {
+      title: String,
+      msg: String,
+      color: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
