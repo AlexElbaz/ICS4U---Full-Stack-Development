8b4777e9b7abcdda1ef69909c4a09affb7d25c02
Alex Elbaz <aelbaz@bayviewglen.ca>
2021-11-07 20:52:10 -0500

    Example Router Project - Learned the basics of the Vue router,

---
+++ Introduction to Vue/router-project/src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1 @click='test'>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'About',
+  methods: {
+    test() {
+      //this.$router.push({path: '/'}) // Home page path.
+      this.$router.push({name: 'Home', params: {username: 'eduardo'}}) // Still goes to home page, just based on path name.
+        // If want to use params, MUST access path through path name or params will be ignored.
+    }
+  }
+
+}
+</script>
